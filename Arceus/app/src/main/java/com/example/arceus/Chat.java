161c90abf43cea4c5b335a8ae5a533dab26bcf57
DeXt3R <swatantradasmgm@gmail.com>
2021-07-15 02:58:03 +0530

    Private Page complete 0.0.3

---
+++ Arceus/app/src/main/java/com/example/arceus/Chat.java
@@ -0,0 +1,42 @@
+package com.example.arceus;
+
+import android.graphics.drawable.Drawable;
+
+public class Chat {
+    String mName,mLastText,mDate,mPendingMessages,mTyping;
+    int mDisplayPic;
+    public Chat(String name,String lastText, String date, String pendingMessages, String typing, int dp)
+    {
+        mName=name;
+        mLastText=lastText;
+        mDate=date;
+        mPendingMessages=pendingMessages;
+        mTyping=typing;
+        mDisplayPic=dp;
+    }
+    public String getNaame()
+    {
+        return mName;
+    }
+    public String getLastText()
+    {
+        return mLastText;
+    }
+    public String getDate()
+    {
+        return mDate;
+    }
+    public String getPendingMessages()
+    {
+        return mPendingMessages;
+    }
+    public String getTyping()
+    {
+        return mTyping;
+    }
+    public int getDP()
+    {
+        return mDisplayPic;
+    }
+
+}
--- Arceus/app/src/main/java/com/example/arceus/MainActivity.java
+++ Arceus/app/src/main/java/com/example/arceus/MainActivity.java
@@ -2,7 +2,17 @@ package com.example.arceus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.text.TextPaint;
+import android.util.Size;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.online);
+        textView.setText("ONLINE".toUpperCase());
+
+        TextPaint paint = textView.getPaint();
+        float width = paint.measureText("ONLINE NEW NEW");
+
+        Shader textShader = new LinearGradient(0, 0, width, textView.getTextSize(),
+                new int[]{
+                        Color.parseColor("#eb1133"),
+                        Color.parseColor("#e74f1c"),
+                        Color.parseColor("#d68407"),
+                }, null, Shader.TileMode.CLAMP);
+        textView.getPaint().setShader(textShader);
+        textView.setTextColor(Color.parseColor("#eb1133"));
+
+        ArrayList<Chat> block=new ArrayList<Chat>();
+        block.add(new Chat("Siddharth","PUBG aa bsdk","6:45 PM","3","",R.drawable.sid));
+        block.add(new Chat("Raj","Delete maar de bhai pls","8:32 AM","5","",R.drawable.rajni));
+        block.add(new Chat("Ayanabha","Bahut paelenge bsdk","8:07 PM","0","",R.drawable.ayanabha));
+        block.add(new Chat("Tanmay","Aareh yaar firse marr gya mai","Just Now","2","typing...",R.drawable.tanmay));
+        block.add(new Chat("Subham","Mera aaj net khtm hai","Yesterday","1","",R.drawable.subham));
+        block.add(new Chat("Arunesh","DSA wala kitna door takk hua?","Just Now","1","typing...",R.drawable.arunesh));
+
+        ListView chats=findViewById(R.id.list);
+        ChatAdapter details=new ChatAdapter(this,block);
+        chats.setAdapter(details);
     }
 }
