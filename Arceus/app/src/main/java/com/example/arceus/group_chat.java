cf4f9d2a7553271cc75ba014d6d5e7a50e7f8587
Subham <subhamsasmal02@gmail.com>
2021-07-16 20:35:54 +0530

    Included chat layout

---
+++ Arceus/app/src/main/java/com/example/arceus/group_chat.java
@@ -0,0 +1,14 @@
+package com.example.arceus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class group_chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+    }
+}
